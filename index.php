31e5b3444109d86e7404fc6e145ad1c0ac4313a0
arjenbos <2047529@talnet.nl>
2020-09-02 15:36:38 +0200

    Lesopdracht 2 PHP

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/* $getal = 11;
+
+echo var_dump(is_int($getal));
+*/
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+/*
+echo var_dump($klas);
+
+
+foreach ($klas as $a) {
+
+  echo $a, PHP_EOL;
+
+}
+*/
+$bucketlist = [];
+$hvlactiviteiten = readline("Hoeveel activiteiten wil je toevoegen aan de lijst?");
+
+for ($i=0; $i < $hvlactiviteiten ; $i++) {
+
+  $bucketlist[$i] = readline("Welke activiteit wil je toevoegen?");
+
+}
+
+var_dump($bucketlist);
+
+foreach ($bucketlist as $b) {
+
+  echo $b, PHP_EOL;
+
+}
+
+
+
+
+
+ ?>
